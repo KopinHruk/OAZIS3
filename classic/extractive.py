c5b2e9a57d241e3a231d816971f478aacd859c70
Kopin Hruk <kopinhruk@gmail.com>
2021-11-19 20:38:03 +0300

    Initial commit; Base functionality is created;

---
+++ classic/extractive.py
@@ -0,0 +1,141 @@
+import nltk
+import math
+import numpy as np
+from tqdm import tqdm
+from nltk.tokenize import RegexpTokenizer
+
+
+def calculate_document_position(text, paragraph_index, sentence_index):
+    symbols_before = 0
+    paragraphs = text.split('.\n')
+    for paragraph in paragraphs[:paragraph_index]:
+        symbols_before += len(paragraph)
+
+    symbols_before += get_count_symbols_before(paragraphs[paragraph_index], sentence_index)
+    position = 1 - (symbols_before) / (len(text))
+    return position
+
+
+def get_count_symbols_before(paragraph, sentence_index):
+    sent_text = nltk.sent_tokenize(paragraph)  # this gives us a list of sentences
+    # now loop over each sentence and tokenize it separately
+    symbols_before = 0
+    for sentence in sent_text[:sentence_index]:
+        symbols_before += len(sentence)
+
+    return symbols_before
+
+
+def calculate_paragraph_position(paragraph, sentence_index):
+    symbols_before = get_count_symbols_before(paragraph, sentence_index)
+
+    position = 1 - (symbols_before) / (len(paragraph))
+    return position
+
+
+def get_word_frequency(word, text):
+    words = nltk.tokenize.word_tokenize(text)
+    fdist = nltk.FreqDist(words)
+
+    return fdist[word]
+
+
+def get_max_frequency(text):
+    words = nltk.tokenize.word_tokenize(text)
+    fdist = nltk.FreqDist(words)
+
+    return max(fdist.values())
+
+
+def get_IDF(word):
+    return 1
+
+
+class ProcessDocs:
+    def __init__(self, documents, number_of_sentences=10, number_of_key_words=10):
+        self.documents = documents
+        self.number_of_sentences = number_of_sentences
+        self.number_of_key_words = number_of_key_words
+
+    def get_IDF(self, word):
+        num_docs_with_word = 0
+        for document in self.documents:
+            tokenized_text = nltk.word_tokenize(document)
+            if word in tokenized_text:
+                num_docs_with_word += 1
+
+        return len(self.documents) / num_docs_with_word
+
+    def _summarize_document(self, document):
+        sent_text = nltk.sent_tokenize(document)
+        sentences_score = []
+        for sentence in sent_text:
+            sentence_score = []
+            tokenized_sentence = nltk.word_tokenize(sentence)
+            for word in tokenized_sentence:
+                frequency_in_sen = get_word_frequency(word, sentence)
+                frequency_in_doc = get_word_frequency(word, document)
+                max_frequency = get_max_frequency(document)
+                word_score = 0.5 * (1 + frequency_in_doc / max_frequency) * math.log(self.get_IDF(word))
+                score = frequency_in_sen * word_score
+                sentence_score.append(score)
+
+            sentences_score.append(sum(sentence_score))
+
+        # print(sentences_score)
+        sentences_score = np.array(sentences_score)
+        ind = np.argpartition(sentences_score, -self.number_of_sentences)[-self.number_of_sentences:]
+        ind = ind[np.argsort(sentences_score[ind])]
+
+        summarized_doc = ' '.join(np.array(sent_text)[ind])
+        return summarized_doc
+
+
+    def _get_key_words(self, document):
+        words_scores = []
+        tokenized_text = np.array(nltk.word_tokenize(document))
+        for word in tokenized_text:
+            frequency_in_doc = get_word_frequency(word, document)
+            max_frequency = get_max_frequency(document)
+            word_score = 0.5 * (1 + frequency_in_doc / max_frequency) * self.get_IDF(word)
+            words_scores.append(word_score)
+
+        words_scores = np.array(words_scores)
+        ind = np.argpartition(words_scores, -self.number_of_key_words)[-self.number_of_key_words:]
+        ind = ind[np.argsort(words_scores[ind])]
+
+        return tokenized_text[ind]
+
+
+
+    def summarize(self):
+        summarized_docs = []
+        key_words = []
+        for document in tqdm(self.documents):
+            summarized_docs.append(self._summarize_document(document))
+            key_words.append(self._get_key_words(document))
+
+        return summarized_docs, key_words
+
+
+if __name__ == '__main__':
+    documents = [
+        '''Реферат по страноведению
+Тема: «Бесплатный парк Варошлигет глазами современников»
+Северное полушарие, на первый взгляд, начинает небольшой бассейн нижнего Инда. Тюлень интуитивно понятен. Независимое государство выбирает шведский цикл, хотя все знают, что Венгрия подарила миру таких великих композиторов как Ференц Лист, Бела Барток, Золтан Кодай, режиссеров Иштвана Сабо и Миклоша Янчо, поэта Шандора Пэтефи и художника Чонтвари. Кукуруза изящно превышает антарктический пояс. Дождливая погода отталкивает архипелаг.
+Бахрейнский динар декларирует широколиственный лес. Глауберова соль существенно входит живописный рельеф. Большое Медвежье озеро погранично. Кристаллический фундамент неравномерен. На улицах и пустырях мальчики запускают воздушных змеев, а девочки играют деревянными ракетками с многоцветными рисунками в ханэ, при этом древняя платформа с сильно разрушенными  складчатыми образованиями доступна.
+Рыболовство вразнобой просветляет небольшой парк с дикими животными к юго-западу от Манамы. Административно-территориальное деление применяет урбанистический антарктический пояс. В ресторане стоимость обслуживания (15%) включена в счет; в баре и кафе - 10-15% счета только за услуги официанта; в такси - чаевые включены в стоимость проезда, тем не менее знаменитый Фогель-маркет на Оудевард-плаатс входит действующий вулкан Катмаи, местами  ширина достигает 100 метров.''',
+        '''Реферат по страноведению
+Тема: «Уличный средиземноморский кустарник: методология и особенности»
+Для гостей открываются погреба Прибалатонских винодельческих хозяйств, известных отличными сортами вин "Олазрислинг" и "Сюркебарат", в этом же году королевство мгновенно. Закрытый аквапарк, куда входят Пик-Дистрикт, Сноудония и другие многочисленные национальные резерваты природы и парки, текстологически оформляет бесплатный круговорот машин вокруг статуи Эроса. Мохово-лишайниковая растительность изящно просветляет небольшой расовый состав, хотя, например, шариковая ручка, продающаяся в Тауэре с изображением стражников Тауэра и памятной надписью, стоит 36 $ США.
+Динарское нагорье вызывает крестьянский ледостав. Бамбуковый медведь панда изменяем. Отгонное животноводство, по определению, точно притягивает распространенный термальный источник. Горная тундра, куда входят Пик-Дистрикт, Сноудония и другие многочисленные национальные резерваты природы и парки, начинает материк.
+Щебнистое плато дорого. Тасмания, при том, что королевские полномочия находятся в руках исполнительной власти - кабинета министров, отражает цикл. Несладкое слоеное тесто, переложенное соленым сыром под названием "сирене", просветляет распространенный знаменитый Фогель-маркет на Оудевард-плаатс, например, "вентилятор" обозначает "веер-ветер", "спичка" - "палочка-чирк-огонь".'''
+    ]
+
+    b = ProcessDocs(documents, number_of_sentences=2)
+    mini_docs, key_words = b.summarize()
+
+    [print(mini_doc) for mini_doc in mini_docs]
+    [print(doc_key_words) for doc_key_words in key_words]
+
+
---
+++ ml/abstract.py
@@ -0,0 +1,26 @@
+from transformers import pipeline
+
+
+def get_abstract_summary(text):
+    summarizer = pipeline("summarization", model='cointegrated/rut5-base-absum')
+
+    summary_text = summarizer(text, max_length=100, min_length=5, do_sample=False)[0]['summary_text']
+
+    return summary_text
+
+
+if __name__ == '__main__':
+    text1 = """One month after the United States began what has become a troubled rollout of a national COVID 
+    vaccination campaign, the effort is finally gathering real steam. Close to a million doses -- over 951,000, 
+    to be more exact -- made their way into the arms of Americans in the past 24 hours, the U.S. Centers for Disease 
+    Control and Prevention reported Wednesday. That's the largest number of shots given in one day since the rollout 
+    began and a big jump from the previous day, when just under 340,000 doses were given, CBS News reported. That 
+    number is likely to jump quickly after the federal government on Tuesday gave states the OK to vaccinate anyone 
+    over 65 and said it would release all the doses of vaccine it has available for distribution. Meanwhile, 
+    a number of states have now opened mass vaccination sites in an effort to get larger numbers of people 
+    inoculated, CBS News reported. """
+
+    text2 = '''А русский язык? - спросил Крапивин. -Он работает, на счет этого можете не переживать. Все будет в лучшем
+            виде! - Ответил Влад.'''
+    temp = get_abstract_summary(text2)
+    print(temp)
